a31bb1eda8160610e2a1538dd01bbaae2b76101e
Wenjie <xuwenjie8866@gmail.com>
2017-10-25 17:39:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!";
   }
 }
