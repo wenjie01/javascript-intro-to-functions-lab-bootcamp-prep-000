86b2d97654306f97e4e03011c6f2d9ee88a2d678
Wenjie <xuwenjie8866@gmail.com>
2017-10-25 17:52:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,14 +11,3 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if  (string === string.toUpperCase()) {
-    return 'YES INDEED!';
-  }
-}
-
-
-function sayHiToGrandma(string) {
-  if (string == 'I love you, Grandma.') {
-    return "I love you, too."
-  }
-}
